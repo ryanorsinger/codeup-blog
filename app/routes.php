32ae25c044f8964401bcfda1ceed9ba1450ca935
vagrant <vagrant@quantal.codeup.dev>
2014-04-01 13:27:20 -0500

    second commit to test

--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,13 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+
+/* Uncomment to Log Eloquent Queries */
+Event::listen('illuminate.query', function($sql, $bindings, $time){
+  Log::info($sql);
+  Log::info(implode($bindings, ', '));
+});
+
 Route::get('/', 'HomeController@showWelcome');
 
 Route::resource('posts', 'PostsController');
